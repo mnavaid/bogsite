531bb8eb429571caf39519d9aff3cc50710763f2
Navaid <akhtar.navaid@gmail.com>
2021-09-15 11:30:17 +0530

    Commit on 140921

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,19 @@
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
 
 # Home View
 def home(request):
   return render(request,'blog/home.html')
+
+# About View
+def about(request):
+  return render(request,'blog/about.html')
+
+# Contact View
+def contact(request):
+  return render(request,'blog/contact.html')
+
+# Dashboard View
+def dashboard(request):
+  return render(request,'blog/dashboard.html')
+
--- blogsite/urls.py
+++ blogsite/urls.py
@@ -13,11 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django import urls
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('users/',include('users.urls')),
 ]
